02c1d1e4ede322ca132a4b219306046eee447f1a
om tiwari <omtiwari888973@gmail.com>
2025-09-19 00:48:32 +0530

    pattern

---
+++ lect7/hello.c
@@ -0,0 +1,99 @@
+// #include <stdio.h>
+// int main(){
+
+
+//      int n=234;
+//     int count=0;
+//     if(n==0){
+//         count=1;
+
+//     }
+//     while (n>0)
+//     {
+//        count++;
+//        n=n/10;
+
+//     }
+// }
+
+
+
+//   1 to 100
+
+//    int n=123 => 3
+//    45666
+
+//    123
+
+//     for(int i=0;i<=10;i++){
+//         // printf("%d",i);
+//         if(i%2==0){
+//             // break;
+//             continue;
+//         }
+//         printf("%d",i);
+//     }
+// }
+
+
+    // int n=11
+    
+    // int isPrime=1;
+    // if(n<=1){
+    //     printf("prime no nhi hai");
+    //     return 0;
+    // }
+    // for(int i=2;i<n;i++){
+    //     if(n%i==0){
+    //         isPrime=0;
+    //         break;
+    //     }
+    // }
+    // if(isPrime){
+    //     printf("prime no haiii");
+
+    // }else
+    // {
+    //     printf("prime no nhii hai");
+    // }
+
+
+
+//     int n=11;
+//     int count =0;
+//     if(n==0){
+//         count=1;
+//     }           
+//     while (n>0)
+//     {  count++;
+//     n=n/10;
+
+//     }
+// }
+
+
+//     int t1=0;
+//     int t2=1;
+//     int next;
+//     for(int i=1;i<=n;i++){
+//         printf("%d",t1);
+//         next=t1+t2;
+//         t1=t2;
+//         t2=next;
+//     }
+// }
+
+
+
+    //   int n=6;
+    //   int t1=0;
+    //   int t2=1;
+    //    int next;
+    //    for(int i=1;i<=n;i++){
+    //     printf("%d",t1);
+    //     next=t1+t2;
+    //      t1=t2;
+    //      t2=next;
+    //   }
+    //    return 0;
+  // }
